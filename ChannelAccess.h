eb55c4d542920479da8034bd48552ca54b0f1899
carl <carl>
2004-02-25 20:57:58 +0000

    Initial revision

---
+++ ChannelAccess.h
@@ -0,0 +1,44 @@
+#ifndef channelaccess_h
+#define channelaccess_h
+
+class Channel;
+
+class ChannelAccess { 
+
+public:
+
+	// Constructor
+	ChannelAccess( void );
+
+	// Destructor
+	virtual ~ChannelAccess( void );
+
+	void Initialize( void );
+	bool IsInitialized( void ) const;
+
+	void SetDefaultTimeouts( void );
+
+	void SetSearchTimeout( double );
+	double GetSearchTimeout( void ) const;
+	int WaitForSearch( void ) const;
+
+	void SetGetTimeout( double );
+	double GetGetTimeout( void ) const;
+	int WaitForGet( void ) const;
+
+	void SetPutTimeout( double );
+	double GetPutTimeout( void ) const;
+	int WaitForPut( void ) const;
+
+private:
+	static bool CA_Initialized;
+
+	double MCA_SEARCH_TIMEOUT;
+	double MCA_GET_TIMEOUT;
+	double MCA_PUT_TIMEOUT;
+
+
+};
+
+#endif // channelaccess_h
+
---
+++ mcamonevents.m
@@ -0,0 +1,19 @@
+function varargout = mcamonevents(varargin)
+%MCAMONEVENTS returns the number of monitor events which have
+%             occurred for channels since the last call to MCACACHE.
+%
+% [HANDLES, EVENTCOUNT] = MCAMONEVENTS
+%    Returns handles and event counts for all open channels
+%
+% EVENTCOUNT = MCAMONEVENTS(HANDLES)
+%    Returns event counts for specified channel(s)
+%
+% See also MCAMON, MCACACHE, MCAGET, MCACLEARMON
+
+[handles, count] = mca(510);
+if nargin == 0
+    varargout{1} = handles;
+    varargout{2} = count;
+elseif nargin > 0 & isnumeric(varargin{1})
+    varargout{1} = count(find(handles==varargin{1}));
+end
