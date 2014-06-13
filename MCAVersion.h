67df8333d06193e5f5693c1fc484a3666478a87c
kasemir <kasemir>
2014-06-13 14:07:28 +0000

    Windows 7 compilation, V 4.4

--- MCAVersion.h
+++ MCAVersion.h
@@ -1,2 +1,2 @@
 // Version string
-#define MCA_VERSION "4.3"
+#define MCA_VERSION "4.4"
--- timereval.cpp
+++ timereval.cpp
@@ -17,7 +17,7 @@ struct TimerEvent
     UINT TimerID;
     struct TimerEvent *Next;
     struct TimerEvent *Prev;
-}; 
+};
 
 static struct TimerEvent *EventListHead = NULL;
 
@@ -29,7 +29,7 @@ struct TimerEvent *FindEventByTimerID( UINT TimerID, struct TimerEvent *Head)
     {   if(ThisOne->TimerID == TimerID)
             return(ThisOne);
         else
-            ThisOne = ThisOne->Next;       
+            ThisOne = ThisOne->Next;
     }
     return(ThisOne);
 }
@@ -63,12 +63,12 @@ void RemoveFromList(struct TimerEvent * EventToRemove)
     }
     else // EventToRemove is NOT the Head
     {   if(!EventToRemove->Next) // EventToRemove is the last element
-        {   (EventToRemove->Prev)->Next = NULL; 
+        {   (EventToRemove->Prev)->Next = NULL;
         }
         else // There are elements before and after EventToRemove - relink
         {   (EventToRemove->Next)->Prev = EventToRemove->Prev;
             (EventToRemove->Prev)->Next = EventToRemove->Next;
-        }   
+        }
     }
     mxFree(EventToRemove->MatlabCallbackString);
     mxFree(EventToRemove);
@@ -77,7 +77,7 @@ void RemoveFromList(struct TimerEvent * EventToRemove)
 static void ClearEventList()
 {   while(EventListHead)
         {   KillTimer(NULL,EventListHead->TimerID);
-            RemoveFromList(EventListHead);      
+            RemoveFromList(EventListHead);
         }
 }
 
@@ -94,13 +94,13 @@ void __stdcall  CallbackFncnSingle( HWND hwnd,   UINT uMsg,   UINT idEvent,  DWO
     // It runs only once: after completion it destroys the timer it is
     // associated with and removes itself from the linked list of scheduled events
     struct TimerEvent *PEvent;
-    
+
     // Search global linked list of events for the matching TimerID
     PEvent = FindEventByTimerID( idEvent, EventListHead);
-    
+
     // Execute commands stored in MatlabCallbackString field in MATLAB 'base' workspace
     mexEvalString(PEvent->MatlabCallbackString);
-    
+
     // After completion - KillTimer and remove the Event from the list
     KillTimer(NULL,idEvent);
     RemoveFromList(PEvent);
@@ -111,10 +111,10 @@ void __stdcall  CallbackFncnMultiple( HWND hwnd,   UINT uMsg,   UINT idEvent,  D
     // It runs infinite number of times.
 
     struct TimerEvent *PEvent;
-    
+
     // Search global linked list of events for the matching TimerID
     PEvent = FindEventByTimerID( idEvent, EventListHead);
-    
+
     // Execute commands stored in MatlabCallbackString field in MATLAB 'base' workspace
     mexEvalString(PEvent->MatlabCallbackString);
 
@@ -128,7 +128,7 @@ void mexFunction(	int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     char* cbs;
     struct TimerEvent* PEvent;
     mexAtExit(ClearEventList);
-    
+
     commandswitch = (int)mxGetScalar(prhs[0]);
     switch(commandswitch)
     {   case 1: //Add single call timer
@@ -137,59 +137,59 @@ void mexFunction(	int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
             cbs = (char*)mxMalloc(buflen);
             mxGetString(prhs[2], cbs ,buflen);
             mexMakeMemoryPersistent(cbs);
-            thandle=SetTimer(NULL,NULL,ms_delay,CallbackFncnSingle);
+            thandle=SetTimer(NULL,NULL,ms_delay,(TIMERPROC) &CallbackFncnSingle);
             AddToList(thandle,cbs);
-            plhs[0]=mxCreateScalarDouble(thandle);
-            
+            plhs[0]=mxCreateDoubleScalar(thandle);
+
         break;
-        
+
         case 2: //Add multiple call timer
             ms_delay = (int)mxGetScalar(prhs[1]);
             buflen = (mxGetM(prhs[2]) * mxGetN(prhs[2]) * sizeof(mxChar)) + 1;
             cbs = (char*)mxMalloc(buflen);
             mxGetString(prhs[2], cbs ,buflen);
             mexMakeMemoryPersistent(cbs);
-            thandle=SetTimer(NULL,NULL,ms_delay,CallbackFncnMultiple);
+            thandle=SetTimer(NULL,NULL,ms_delay,(TIMERPROC) &CallbackFncnMultiple);
             AddToList(thandle,cbs);
-            plhs[0]=mxCreateScalarDouble(thandle);
+            plhs[0]=mxCreateDoubleScalar(thandle);
         break;
-        
-        case 3: 
+
+        case 3:
             // Kill all existing timers before starting a new one
             // timereval(3,type,delay,callback);
             ClearEventList();
-            option = (int)mxGetScalar(prhs[1]);         
+            option = (int)mxGetScalar(prhs[1]);
             ms_delay = (int)mxGetScalar(prhs[2]);
             buflen = (mxGetM(prhs[3]) * mxGetN(prhs[3]) * sizeof(mxChar)) + 1;
             cbs = (char*)mxMalloc(buflen);
             mxGetString(prhs[3], cbs ,buflen);
             mexMakeMemoryPersistent(cbs);
-            
+
             if(option==1)
-                thandle=SetTimer(NULL,NULL,ms_delay,CallbackFncnSingle);
+                thandle=SetTimer(NULL,NULL,ms_delay,(TIMERPROC) &CallbackFncnSingle);
             else if(option==2)
-                thandle=SetTimer(NULL,NULL,ms_delay,CallbackFncnMultiple);
-                
+                thandle=SetTimer(NULL,NULL,ms_delay,(TIMERPROC) &CallbackFncnMultiple);
+
             AddToList(thandle,cbs);
-            plhs[0]=mxCreateScalarDouble(thandle);
-            
-        break;   
-        
+            plhs[0]=mxCreateDoubleScalar(thandle);
+
+        break;
+
         case 4:
             PrintEventList();
-        
-            
-        break; 
-        
+
+
+        break;
+
         case 5:
-            tofind = (int)mxGetScalar(prhs[1]);  
+            tofind = (int)mxGetScalar(prhs[1]);
             PEvent = FindEventByTimerID( tofind, EventListHead);
             if(PEvent)
             {   KillTimer(NULL,PEvent->TimerID);
                 RemoveFromList(PEvent);
             }
-                 
-        break;    
+
+        break;
 
         case 6:
             // timereval(6,OldTimerID,type,delay,callback);
@@ -200,25 +200,24 @@ void mexFunction(	int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
                 RemoveFromList(PEvent);
             }
             option = (int)mxGetScalar(prhs[2]);
-            
+
             ms_delay = (int)mxGetScalar(prhs[3]);
             buflen = (mxGetM(prhs[4]) * mxGetN(prhs[4]) * sizeof(mxChar)) + 1;
             cbs = (char*)mxMalloc(buflen);
             mxGetString(prhs[4], cbs ,buflen);
             mexMakeMemoryPersistent(cbs);
-            
+
             if(option==1)
-                thandle=SetTimer(NULL,NULL,ms_delay,CallbackFncnSingle);
+                thandle=SetTimer(NULL,NULL,ms_delay,(TIMERPROC) &CallbackFncnSingle);
             else if(option==2)
-                thandle=SetTimer(NULL,NULL,ms_delay,CallbackFncnMultiple);
-                
+                thandle=SetTimer(NULL,NULL,ms_delay,(TIMERPROC) &CallbackFncnMultiple);
+
             AddToList(thandle,cbs);
-            plhs[0]=mxCreateScalarDouble(thandle);
-            
-        break;   
-        
+            plhs[0]=mxCreateDoubleScalar(thandle);
+
+        break;
 
     }
-            
-        
+
+
 }
